e4d197776cb2bc4f3f17e4e7d8c8989b1f820d66
RulosS290 <146139366+RulosS290@users.noreply.github.com>
2024-09-12 13:42:56 -0500

    Add files via upload

---
+++ src/main/java/co/edu/escuelaing/parcial/parcial/Parcial.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.escuelaing.parcial.parcial;
+
+/**
+ *
+ * @author daniel.torres-ac
+ */
+public class Parcial {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
